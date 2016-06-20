506945b308eecb0f9e6ee7e92ade1c657a53f3de
niqdev <niqdev@gmail.com>
2016-06-20 08:41:17 +0200

    update App container

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,25 @@
-/*
 import React, {Component, PropTypes} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
+import {Link, IndexLink} from 'react-router';
 import * as CounterActions from '../actions/CounterActions';
 import Counter from '../components/Counter';
 import Footer from '../components/Footer';
 
 export default class App extends Component {
   render() {
+    const ACTIVE = {color: 'red'};
     const {counter, actions} = this.props;
     return (
       <div className="main-app-container">
+        <ul>
+          <li><Link to="/" activeStyle={ACTIVE}>/</Link></li>
+          <li><IndexLink to="/" activeStyle={ACTIVE}>IndexLink</IndexLink></li>
+          <li><Link to="/gallery" activeStyle={ACTIVE}>Gallery</Link></li>
+        </ul>
         <div className="main-app-nav">Simple Redux Boilerplate</div>
         <Counter counter={counter} actions={actions}/>
+        {this.props.children}
         <Footer />
       </div>
     );
@@ -40,24 +47,3 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(App);
-*/
-
-import React, {Component} from 'react';
-import {Link, IndexLink} from 'react-router';
-
-export default class App extends Component {
-  render() {
-    const ACTIVE = {color: 'red'};
-    return (
-      <div>
-        <h1>APP</h1>
-        <ul>
-          <li><Link to="/" activeStyle={ACTIVE}>/</Link></li>
-          <li><IndexLink to="/" activeStyle={ACTIVE}>IndexLink</IndexLink></li>
-          <li><Link to="/gallery" activeStyle={ACTIVE}>Gallery</Link></li>
-        </ul>
-        {this.props.children}
-      </div>
-    );
-  }
-}
