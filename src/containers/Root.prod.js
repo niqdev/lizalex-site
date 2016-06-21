5ea9e2c49840a3365c6b13ef718994eddce249ec
niqdev <niqdev@gmail.com>
2016-06-21 08:25:58 +0200

    add routes prod configuration

--- src/containers/Root.prod.js
+++ src/containers/Root.prod.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
-import App from './App';
+import {Router, browserHistory} from 'react-router';
+import routes from '../routes/index';
 
-// TODO router
 export default class Root extends Component {
   render() {
     return (
       <Provider store={this.props.store}>
-        <App />
+        <Router history={browserHistory} routes={routes}/>
       </Provider>
     );
   }
