104975da48060dc25297c261c60d07670bd33df7
niqdev <niqdev@gmail.com>
2016-06-21 20:40:15 +0200

    update route history prod

--- src/containers/Root.prod.js
+++ src/containers/Root.prod.js
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
-import {Router, browserHistory} from 'react-router';
+
+// https://github.com/reactjs/react-router/blob/master/upgrade-guides/v2.0.0.md#using-custom-histories
+// http://jaketrent.com/post/pushstate-webpack-dev-server
+// https://github.com/reactjs/react-router/blob/master/docs/guides/Histories.md
+// https://github.com/reactjs/react-router-tutorial
+// https://medium.com/@dabit3/beginner-s-guide-to-react-router-53094349669
+
+// browserHistory needs a server
+import { Router, useRouterHistory } from 'react-router'
+import { createHashHistory } from 'history'
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 import routes from '../routes/index';
 
 export default class Root extends Component {
   render() {
     return (
       <Provider store={this.props.store}>
-        <Router history={browserHistory} routes={routes}/>
+        <Router history={appHistory} routes={routes}/>
       </Provider>
     );
   }
