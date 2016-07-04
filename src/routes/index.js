d90b647943d10ab189aaa3b84c5866eeb6bdb989
niqdev <niqdev@gmail.com>
2016-07-04 08:47:57 +0200

    handle url not found

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,12 @@ import {FormattedDate} from 'react-intl';
 
 const Index = () => <div>INDEX</div>
 const Gallery = () => <div>GALLERY - <FormattedDate value={Date.now()}/></div>
+const NotFound = () => <div>NOT FOUND</div>
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Index}/>
     <Route path="/gallery" component={Gallery}/>
+    <Route path="*" component={NotFound}/>
   </Route>
 )
