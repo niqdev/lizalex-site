e9eeac571433a14e4c577c47e80c9d40b73bbc8a
niqdev <niqdev@gmail.com>
2016-07-21 19:51:44 +0200

    add cookie

--- src/modules/app/AppComponent.js
+++ src/modules/app/AppComponent.js
@@ -1,10 +1,12 @@
 import React, { PropTypes } from 'react'
 import Navbar from '../navbar/NavbarComponent'
+import Cookie from '../navbar/CookieComponent'
 import './app.scss'
 
 export default function App({ children }) {
   return (
     <div>
+      <Cookie />
       <Navbar />
       {children}
     </div>
