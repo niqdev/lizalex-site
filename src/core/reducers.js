f9be361ecb0668e5035b26e07dc4d5435582fd29
niqdev <niqdev@gmail.com>
2016-07-22 09:18:56 +0200

    update public

--- src/core/reducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import { intlReducer } from 'react-intl-redux'
-
-const rootReducer = combineReducers({
-  intl: intlReducer
-})
-
-export default rootReducer
--- src/modules/index.js
+++
@@ -1,7 +0,0 @@
-import { default as App } from './app/AppComponent'
-import { default as Home } from './home/HomeComponent'
-
-module.exports = {
-  App,
-  Home
-}
