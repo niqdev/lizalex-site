5b2786d34a29810ac060e560d7bb0c73c58241e1
niqdev <niqdev@gmail.com>
2016-08-01 18:56:08 +0200

    add cloudinary demo images

--- src/modules/home/HeaderComponent.js
+++ src/modules/home/HeaderComponent.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Slider from 'react-slick'
 import { FormattedMessage } from 'react-intl'
-import placehold from './img/placehold_1900x700.png'
+// import placehold from './img/placehold_1900x700.png'
 
 export default function HeaderComponent() {
   const settings = {
@@ -16,12 +16,14 @@ export default function HeaderComponent() {
     autoplaySpeed: 3000,
     touchMove: true
   }
+  // TODO iterate array
   return (
     <section className="header">
       <Slider {...settings}>
-        <div><img src={placehold} alt="carousel-1" className="img-carousel" /></div>
-        <div><img src={placehold} alt="carousel-2" className="img-carousel" /></div>
-        <div><img src={placehold} alt="carousel-3" className="img-carousel" /></div>
+        <div><img src="http://res.cloudinary.com/niqdev/image/upload/v1470068480/demo2/DSC_0138_HDR.jpg" alt="carousel-1" className="img-carousel" /></div>
+        <div><img src="http://res.cloudinary.com/niqdev/image/upload/v1470068486/demo2/DSC_0172-Modifica-3-Modifica.jpg" alt="carousel-2" className="img-carousel" /></div>
+        <div><img src="http://res.cloudinary.com/niqdev/image/upload/v1470068487/demo2/DSC_0312.jpg" alt="carousel-3" className="img-carousel" /></div>
+        <div><img src="http://res.cloudinary.com/niqdev/image/upload/v1470068484/demo2/DSC_0329_HDR_3.jpg" alt="carousel-4" className="img-carousel" /></div>
       </Slider>
       <div className="container">
         <div className="text-xs-center m-t-2">
